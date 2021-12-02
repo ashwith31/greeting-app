fda7b3d1cb99b597082dec0db9e4f2d21211faba
ashwith31 <ashwitash@gmail.com>
2021-12-02 21:48:35 +0530

    Added: Code to get greeting message using Service class

--- src/main/java/com/bridgelabz/controller/GreetingAppController.java
+++ src/main/java/com/bridgelabz/controller/GreetingAppController.java
@@ -1,14 +1,24 @@
 package com.bridgelabz.controller;
 
 import com.bridgelabz.Dto.GreetingDto;
+import com.bridgelabz.service.GreetingAppService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class GreetingAppController {
+    @Autowired
+    GreetingAppService greetingAppService;
 
     @GetMapping("")
     public GreetingDto sayHello(@RequestBody GreetingDto greetingDto){
         return greetingDto;
+    }
+
+    //http://localhost:8080/message
+    @GetMapping("/message")
+    public String getGreeting(){
+        return greetingAppService.greet();
     }
 }
---
+++ src/main/java/com/bridgelabz/service/GreetingAppService.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingAppService {
+
+    public String greet(){
+        return "Hello World";
+    }
+}
